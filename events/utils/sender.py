b51dcb43ee373b356392f8f70351bd9a9356cc14
Flict-dev <zhdanom.maksi@gmail.com>
2023-08-14 21:32:18 +0300

    feat(proxy): add proxy class
    Added a class for getting a random proxy from free proxy website
    Added a function to generate random information about the client

--- events/utils/sender.py
+++ events/utils/sender.py
@@ -1,6 +1,10 @@
 from logging import Logger
+from random import choice
+from typing import List
 
 from aiohttp import ClientSession
+from bs4 import BeautifulSoup
+from user_agent import generate_navigator
 
 logger = Logger(__file__)
 
@@ -14,16 +18,65 @@ class Singleton(type):
         return cls._instances[cls]
 
 
-class Sender(metaclass=Singleton):
-    HEADERS = {
-        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36"
-    }
+class ProxyManager:
+    def __init__(self, session: ClientSession) -> None:
+        self.__session = session
+        self._proxy_provider = "https://free-proxy-list.net/"
 
+    async def _get_proxies(self) -> List[str]:
+        result = []
+        async with self.__session.get(self._proxy_provider) as response:
+            if response.status != 200:
+                logger.critical(
+                    f"Free proxy website sent invalid status code {response.status}"
+                )
+                return result
+            html = await response.text()
+        soup = BeautifulSoup(html, "html.parser")
+        proxy_table = soup.find("table", class_="table table-striped table-bordered")
+        if not proxy_table:
+            logger.critical(
+                "The parser crashed with a critical error the structure of the site may have been changed!"
+            )
+            return result
+        table_body = proxy_table.find("tbody")
+        for row in table_body.find_all("tr")[10:]:
+            columns = row.find_all("td")
+            ip = columns[0].text
+            port = columns[1].text
+            protocol = "https" if columns[6].text == "yes" else "http"
+            result.append(f"{protocol}://{ip}:{port}")
+        return result
+
+    async def _check_proxy(self, proxy: str) -> bool:
+        async with self.__session.get(proxy) as response:
+            return response.status == 200
+
+    async def get_proxy(self) -> str | None:
+        proxy_list = await self._get_proxies()
+        if not proxy_list:
+            logger.error("Proxy list not found!")
+            return None
+
+        proxy = choice(proxy_list)
+        while not self._check_proxy(proxy):
+            proxy_list.remove(proxy)
+            if len(proxy_list) == 0:
+                logger.error("Proxy list not found!")
+                return None
+            proxy = choice(proxy_list)
+        return proxy
+
+
+class Sender(metaclass=Singleton):
     def __init__(self) -> None:
         self.__session = ClientSession()
+        self._proxy_manger = ProxyManager(self.__session)
 
     async def get_html(self, url: str, company: str) -> str | None:
-        async with self.__session.get(url, headers=self.HEADERS) as response:
+        proxy = await self._proxy_manger.get_proxy()
+        headers = generate_navigator(os=("win", "mac"))
+        async with self.__session.get(url, headers=headers, proxy=proxy) as response:
             if response.status != 200:
                 logger.error(f"Request error with <{company}> at - {url}")
                 return None
